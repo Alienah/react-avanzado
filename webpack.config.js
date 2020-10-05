1b11aca077804a0399d285a7d4378b695aaaa3b4
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-05 16:57:51 +0200

    Install webpack, react and babel and initial config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  // En el objeto le decimos que el output va a estar en un archivo app.bundle.js
+  output: {
+    filename: 'app.bundle.js',
+  },
+  // Además le podemos decir qué plugins queremos que utilice
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ],
+  //Le añadimos una nueva configuración
+  module: {
+    // Le vamos a especificar las reglas que queremos que tengan nuestros módulos
+    rules: [
+      {
+        // Le decimos que tiene que usar esta regla para todos los archivos que sean de javascript
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          //le decimos que en esos casos use este loader
+          loader: 'babel-loader',
+          options: {
+            // Con la opción de este preset
+            presets: [
+              '@babel/preset-env','@babel/preset-react'
+            ],
+          },
+        },
+      }
+  
+    ]
+  }
+}
