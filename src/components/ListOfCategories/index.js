8b9ed4ea1a103013204579ef67d3b1e033830a1e
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-06 14:05:50 +0200

    Add categories info importing it from db json

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
+import { categories } from '../../../api/db.json';
 
 export const ListOfCategories = () => (
   <List>
     {
-        [1, 2, 3, 4, 5, 6].map((category) => <Item key={category}><Category /></Item>)
-      }
+      categories.map((category) => {
+        const {
+          cover, emoji, name, path,
+        } = category;
+        return (
+          <Item key={category.id}>
+            <Category
+              cover={cover}
+              emoji={emoji}
+              name={name}
+              path={path}
+            />
+          </Item>
+        );
+      })
+    }
   </List>
 );
