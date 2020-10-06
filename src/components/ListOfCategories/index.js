4ab39f5523f159be19e160ce55283632a721970e
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-06 18:02:36 +0200

    Add scroll suscription in useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(() => {
     fetch('https://wowgram-api.vercel.app/categories')
@@ -11,8 +12,22 @@ export const ListOfCategories = () => {
       .then((categoriesData) => setCategories(categoriesData));
   }, []);
 
-  return (
-    <List>
+  useEffect(() => {
+    const onScroll = () => {
+      const newShowFixed = window.scrollY > 200;
+      // Sólo vamos a querer que ocurra cuando ya no está fijo,
+      // para que no cambie el estado continuamente
+      if (showFixed !== newShowFixed) setShowFixed(newShowFixed);
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+    // Para eliminar las suscripciones a eventos
+    return () => document.removeEventListener('scroll', onScroll);
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
       categories.map((category) => {
         const {
@@ -31,5 +46,12 @@ export const ListOfCategories = () => {
       })
     }
     </List>
+  );
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { fadeIn } from '../../styles/animation';
 
 export const List = styled.ul`
   display: flex;
@@ -6,6 +7,24 @@ export const List = styled.ul`
   width: 100%;
   &::-webkit-scrollbar {
     display: none;
+  }
+
+  &.fixed {
+    ${fadeIn({ time: '0.5s' })}
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    /* Esta tres propiedades juntas hacen que se centre horizontalmente */
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    top: -20px;
+    /* Para que se vea un poc más pequeño. La mitad */
+    transform: scale(.5);
+    z-index: 1;
   }
 `;
 
