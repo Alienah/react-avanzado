3e77f27dd0687b6ec4d41f9da81f3e19bdff0ec4
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-06 13:15:21 +0200

    Add global styles an list component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Category } from './components/Category';
+import { ListOfCategories } from './components/ListOfCategories';
+import { GlobalStyle } from './GlobalStyles';
 
 const App = () => (
-  <Category />
+  <div>
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
 );
 
 export default App;
