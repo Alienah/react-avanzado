d967f6298f2b36fc2c11c3b702755aaee2af576a
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-06 19:07:22 +0200

    Add lazy load with hooks

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -3,6 +3,6 @@ import { PhotoCard } from '../PhotoCard';
 
 export const ListOfPhotoCards = () => (
   <ul>
-    {[1, 2, 3].map((photoCard) => <li key={photoCard}><PhotoCard /></li>)}
+    {[1, 2, 3, 4, 5, 6, 7].map((photoCard) => <li key={photoCard}><PhotoCard /></li>)}
   </ul>
 );
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { MdFavoriteBorder } from 'react-icons/md';
-import { ImgWrapper, Img, Button } from './styles';
+import {
+  Article, ImgWrapper, Img, Button,
+} from './styles';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v1555671700/category_dogs.jpg';
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => (
-  <article>
-    <a href={`/detail/${id}`}>
-      <ImgWrapper>
-        <Img src={src} alt="" />
-      </ImgWrapper>
-    </a>
-    <Button type="button">
-      <MdFavoriteBorder size="32px" />{likes} likes!
-    </Button>
-  </article>
-);
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    // Un observer que te indica si es visible un elemento, entre otras cosas
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        setShow(true);
+        // Porque sólo queremos que se actualice un vez
+        // y una vez que está mostrándose, se puede desconectar:
+        observer.disconnect();
+      }
+    });
+    observer.observe(element.current);
+  }, [element]);
+
+  return (
+    <Article ref={element}>
+      {
+        // Si está en el viewPort se renderiza
+        show
+        && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="" />
+            </ImgWrapper>
+          </a>
+          <Button type="button">
+            <MdFavoriteBorder size="32px" />{likes} likes!
+          </Button>
+        </>
+        )
+
+      }
+    </Article>
+  );
+};
