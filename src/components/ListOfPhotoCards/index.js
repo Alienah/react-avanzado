9a2de3584fa15b4ab7c4731f776dfa2ac4f0589f
Aida Albarrán Blanco <aidalbla@gmail.com>
2020-10-07 11:41:44 +0200

    Add local storage example

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -3,6 +3,6 @@ import { PhotoCard } from '../PhotoCard';
 
 export const ListOfPhotoCards = () => (
   <ul>
-    {[1, 2, 3, 4, 5, 6, 7].map((photoCard) => <li key={photoCard}><PhotoCard /></li>)}
+    {[1, 2, 3, 4, 5, 6, 7].map((photoCard) => <li key={photoCard}><PhotoCard id={photoCard} /></li>)}
   </ul>
 );
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { MdFavoriteBorder } from 'react-icons/md';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 import {
   Article, ImgWrapper, Img, Button,
 } from './styles';
@@ -9,6 +9,18 @@ const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_150/v15
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const element = useRef(null);
   const [show, setShow] = useState(false);
+  const key = `like-${id}`;
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      // Lo tenemos que parsear para que en vez de string nos devuelva el valor true o false
+      return JSON.parse(like);
+    } catch (error) {
+      /* eslint-disable-next-line no-console */
+      console.error(error);
+      return false;
+    }
+  });
 
   useEffect(() => {
     // Envolvemos la ternaria en una promesa
@@ -33,6 +45,18 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     });
   }, [element]);
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (error) {
+      /* eslint-disable-next-line no-console */
+      console.error(error);
+    }
+  };
+
   return (
     <Article ref={element}>
       {
@@ -45,8 +69,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               <Img src={src} alt="" />
             </ImgWrapper>
           </a>
-          <Button type="button">
-            <MdFavoriteBorder size="32px" />{likes} likes!
+          <Button onClick={() => setLocalStorage(!liked)} type="button">
+            <Icon size="32px" />{likes} likes!
           </Button>
         </>
         )
